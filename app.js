d7ec0dfdeee85b9365102ba1944e2defcc5b48a5
Nare <narela00528@gmail.com>
2024-05-27 19:08:32 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let precioProducto = document.getElementById
+("precioProducto");
+
+let titulo = document.querySelector
+("#nombreProducto")
+
+precioProducto.innerText ="precio : $450"
+
+nombreProducto.style.color= "blue";
+nombreProducto.style.fontsize="3em";
+nombreProducto.style.fontweight="bold";
+
+precioProducto.style.color="purple";
+precioProducto.style.font= "News Roman";
